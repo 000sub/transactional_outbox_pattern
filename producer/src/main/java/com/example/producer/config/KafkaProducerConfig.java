1e5594c9df187fa62d95f037cc0885dfb090be0c
dmong <dt2980_hee@naver.com>
2024-09-24 16:45:56 +0900

    외부 의존성 설정 파일 추가

---
+++ producer/src/main/java/com/example/producer/config/KafkaProducerConfig.java
@@ -0,0 +1,46 @@
+package com.example.producer.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+@Configuration
+@EnableKafka
+public class KafkaProducerConfig {
+
+    private final String kafkaBootstrapUri;
+
+    public KafkaProducerConfig(
+            @Value("${spring.kafka.producer.bootstrap-servers}") String kafkaBootstrapUri
+    ) {
+        this.kafkaBootstrapUri = kafkaBootstrapUri;
+    }
+
+    public Map<String, Object> producerConfig() {
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapUri);
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
+
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(this.producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(this.producerFactory());
+    }
+}
